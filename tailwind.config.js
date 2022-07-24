6bbf0739df338e8de9b916473513f96d299344da
ErikKaum <erik.kaum@gmail.com>
2022-07-24 09:46:23 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'cf-light-blue': '#BED7D8',
+      'cf-cream': '#EFF5DD',
+      'cf-red': '#EC6A5C',
+    },
+  },
+  plugins: [],
+}
